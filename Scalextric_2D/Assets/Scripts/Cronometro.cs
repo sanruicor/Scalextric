08a6cb21e75ccbf4f5ce755aa7a2f1f1da36aa0d
Hugo Otero <hugossdss@gmail.com>
2026-01-19 19:19:01 +0100

    Actualizacion de cronometro

--- Scalextric_2D/Assets/Scripts/Cronometro.cs
+++ Scalextric_2D/Assets/Scripts/Cronometro.cs
@@ -3,8 +3,12 @@ using TMPro;
 public class Cronometro : MonoBehaviour
 {
     [SerializeField] private TMP_Text cronometroText;
+    [SerializeField] private TMP_Text bestlapText;
+    [SerializeField] private TMP_Text lastlapText;
 
     private float timeElapsed;
+    private float bestLap = Mathf.Infinity;
+    private float lastLap = 0f;
     private int minutes, seconds, cents;
 
     private void Update()
@@ -15,5 +19,27 @@ public class Cronometro : MonoBehaviour
         cents = (int)((timeElapsed - (int)timeElapsed) * 100f);
 
         cronometroText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, cents);
+    }
+
+    public void RegistrarVuelta()
+    {
+        lastLap = timeElapsed;
+        lastlapText.text = "Last: " + TimeFormat(lastLap);
+
+        if (lastLap < bestLap)
+        {
+            bestLap = lastLap;
+            bestlapText.text = "Best: " + TimeFormat(bestLap);
+        }
+        timeElapsed = 0f;
+    }
+
+    private string TimeFormat(float t)
+    {
+        int m = (int)(t / 60f);
+        int s = (int)(t - m * 60f);
+        int c = (int)((t - (int)t) * 100f);
+
+        return string.Format("{0:00}:{1:00}:{2:00}", m, s, c);
     }
 }
